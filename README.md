245dacc65511d885cddbeb8e9e932aff74c08a58
Ashwin Iyer <ashwiniyer1706@gmail.com>
2022-03-01 20:17:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django-REST-API
+
+A simple REST API created using Django and Django Rest Framework following along with the latter's [documentation tutorial](https://www.django-rest-framework.org/tutorial/1-serialization/)
---
+++ snippets/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+# A Serializer is a Form, therefore a ModelSerializer is essentially a ModelForm
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    '''
+    Serializers provide a way of serializing and deserializing the snippet 
+    instances into representations such as json, which work similar to
+    Django's forms. 
+    '''
+    class Meta:
+        models = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
